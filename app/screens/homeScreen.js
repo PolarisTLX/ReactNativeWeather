eddd58df7fb6a5a5d1033bb33785fc6907e89eda
Paul Rail <paul.a.rail@gmail.com>
2019-06-05 12:43:29 -0400

    pulling 7 random cities and temperatures from API openweathermap.org

--- app/screens/homeScreen.js
+++ app/screens/homeScreen.js
@@ -6,8 +6,119 @@ export default class App extends React.Component{
     super(props);
     let navigation = this.props.navigation;
     this.state = {
+      cities: [
+            { name: "London",
+              country: "UK"
+            },
+            { name: "Edinburgh",
+              country: "UK"
+            },
+            { name: "New York",
+              country: "US"
+            },
+            { name: "Texas",
+              country: "US"
+            },
+            { name: "Washington",
+              country: "US"
+            },
+            { name: "Paris",
+              country: "France"
+            },
+            { name: "Doha",
+              country: "Qatar"
+            },
+            { name: "Sydney",
+              country: "Australia"
+            },
+            { name: "Cancun",
+              country: "Mexico"
+            },
+            { name: "Madrid",
+              country: "Spain"
+            },
+            { name: "Berlin",
+              country: "Germany"
+            },
+            { name: "Brussels",
+              country: "Belgium"
+            },
+            { name: "Copenhagen",
+              country: "Denmark"
+            },
+            { name: "Athens",
+              country: "Greece"
+            },
+            { name: "New Delhi",
+              country: "India"
+            },
+            { name: "Dublin",
+              country: "Ireland"
+            },
+            { name: "Rome",
+              country: "Italy"
+            },
+            { name: "Tokyo",
+              country: "Japan"
+            },
+            { name: "Wellington",
+              country: "New Zealand"
+            },
+            { name: "Amsterdam",
+              country: "Netherlands"
+            },
+            { name: "Oslo",
+              country: "Norway"
+            },
+            { name: "Panama City",
+              country: "Panama"
+            },
+            { name: "Lisbon",
+              country: "Portugal"
+            },
+            { name: "Warsaw",
+              country: "Poland"
+            },
+            { name: "Moscow",
+              country: "Russia"
+            }
+      ],
+      list: [],
+      refresh: true
+    };
 
+    // this.fetchCityTemp('London', 'UK');
+    let list = this.getRandom(this.state.cities, 5);
+    console.log(list);
+  }
+
+  getRandom = (array, number) => {
+    let result = new Array(number),
+      length = array.length,
+      taken = new Array(length);
+
+    while(number--) {
+      let x = Math.floor(Math.random() * length);
+      result[number] = array[x in taken ? taken[x] : x];
+      taken[x] = --length in taken ? taken[length] : length;
     }
+    return result;
+  }
+
+  fetchCityTemp = ( city, country ) => {
+    // fetch('http://api/openweathermap/org/data/2.5/weather?q='+city+','+country+'&appid=apikey')
+    fetch('http://api/openweathermap/org/data/2.5/weather?q='+city+','+country+'&appid=f342bc09b6f0bcd7c671338423a7e5cc&units=metric')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      let r = responseJson.main;
+      let obj = responseJson;
+      let city = {
+        name: obj.name,
+        country: country,
+        temp: Math.ceil(r.temp),
+        type: obj.weather[0].main
+      };
+    })
   }
 
   render() {
